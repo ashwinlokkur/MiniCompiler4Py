08c3ebac8e0b0530e055a3b4fb0bba95cf19af1c
Ashwin Lokkur <ash.lokkur@gmail.com>
2017-04-07 19:50:49 +0530

    Add files via upload

---
+++ inputFile.py
@@ -0,0 +1,6 @@
+n=5
+#comm     qsdasd
+  #co mm 2
+    #c omm 3
+for i in range(0, n) :  #testComm 
+    print i   #  testComm 2
---
+++ plyLex.py
@@ -0,0 +1,63 @@
+import ply.lex as lex
+import re
+#import ply.yacc as yacc
+
+tokens = ['NAME','VAR','NUMBER','ID','PLUS','MINUS','MULTIPLY','DIVIDE','COMMA','PARANOPEN','PARANCLOSE','FLOWEROPEN','FLOWERCLOSE','EQUAL','GREATER','LESSER','GREATEREQ','LESSEREQ','EQEQ','NOTEQ','AND','OR']
+reserved = {'if':'IF','for':'FOR','range':'RANGE','print':'PRINT'}
+
+tokens = tokens + list(reserved.values())
+
+t_ignore=' '
+t_ignore_COMMENT=r'[#].*'
+t_PLUS=r'\+'
+t_MINUS=r'\-'
+t_MULTIPLY=r'\*'
+t_DIVIDE=r'/'
+t_EQUAL=r'='
+t_COMMA=r','
+t_PARANOPEN=r'\('
+t_PARANCLOSE=r'\)'
+t_FLOWEROPEN=r'{'
+t_FLOWERCLOSE=r'}'
+t_GREATER=r'>'
+t_LESSER=r'<'
+t_GREATEREQ=r'>='
+t_LESSEREQ=r'<='
+t_EQEQ=r'=='
+t_NOTEQ=r'!='
+t_AND=r'&&'
+t_OR=r'\|\|'
+
+def t_error(t):
+    #print "ERROR"
+    t.lexer.skip(1)
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    if t.value in reserved:
+        t.type=reserved[t.value]
+    else:
+        t.type="VAR"
+    return t       
+'''
+def t_COMMENT(t):
+    r'[#].*'
+    t.lexer.skip(1)
+'''
+lexer = lex.lex()
+
+f = open('inputFile.py')
+data = f.read()
+
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
